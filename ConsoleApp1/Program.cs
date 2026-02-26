bc744e74be455c1049206ee4f9e6bc93371cf2e2
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-02-26 06:08:27 +0200

    Question01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 : Theoretical Questions
+
+            #region Question 01
+
+            //a) A University has Departments. If the university is closed, the departments no longer exist.
+            //Composition Relationship
+            //b) A Driver uses a Car.The driver does not own the car.
+            //Association Relationship
+            //c) A Dog is an Animal.
+            //Inheritance Realatinoship
+            //d) A Team has Players. If the team is deleted, the players still exist.
+            //Aggregation Relationship
+            //e) A method receives a Logger as a parameter and calls it inside the method only.
+            //Dependency Relatinoship
+
+            #endregion
+
+            #endregion
         }
     }
 }
